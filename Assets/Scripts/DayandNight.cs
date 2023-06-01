91f7c370891b6985f522bde380e5b92b01d298d4
Gabe Murray <gabemurray@Gabes-MacBook.local>
2023-06-01 13:03:03 -0400

    Baller

--- Assets/Scripts/DayandNight.cs
+++ Assets/Scripts/DayandNight.cs
@@ -8,11 +8,22 @@ public class DayandNight : MonoBehaviour
     [SerializeField]
     float degpersec=6;
 
+    [SerializeField]
+    Camera cam;
+
     // Update is called once per frame
     void Update()
     {
         rot.x=degpersec*Time.deltaTime;
-        transform.Rotate(rot,Space.World);
+        transform.parent.Rotate(rot,Space.World);
+
+        transform.parent.rotation = Quaternion.AngleAxis(degpersec, transform.parent.right) * transform.parent.rotation;
+        Debug.Log(transform.parent.eulerAngles);
+
+
+        var lerp = Mathf.InverseLerp(-180, 180, transform.parent.eulerAngles.x);
+            Debug.Log(lerp);
+        cam.backgroundColor = new Color(.65f*lerp,.75f*lerp,lerp,1);
         
     }
 }
